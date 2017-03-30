2339395a7841e4b4c6aef75ee43d5e5b2e551a9f
kientranutc <kienkienutc95@gmail.com>
2017-03-30 18:28:12 +0700

    addFrontend

---
+++ public/frontend/assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+   $(".main-slideshow").owlCarousel({ 
+     autoPlay:true,
+      items:1,
+      loop:true,
+      slideSpeed: 300,
+     navigation : true
+    });
+});
