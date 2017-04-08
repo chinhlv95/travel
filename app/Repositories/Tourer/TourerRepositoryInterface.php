f3bb8e4f73d1533a803f8bbaed6bd9c936398c7c
Chinhlv <chinhlv0095@gmail.com>
2017-04-08 14:19:25 +0700

    merge vs kien

--- app/Repositories/Tourer/TourerRepositoryInterface.php
+++ app/Repositories/Tourer/TourerRepositoryInterface.php
@@ -4,5 +4,10 @@ namespace App\Repositories\Tourer;
 
 interface TourerRepositoryInterface
 {
-
+  /**
+  *get list tourer for order
+  *@param integer $order_id
+  *@return mixed
+  */
+  public function getListTourer($order_id);
 }
