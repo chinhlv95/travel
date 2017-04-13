4086228b88edf4e4922329af7b54bfce4054a2f7
Chinhlv <chinhlv0095@gmail.com>
2017-04-13 10:06:01 +0700

    fix tour

--- app/Repositories/Tour/TourEloquentRepository.php
+++ app/Repositories/Tour/TourEloquentRepository.php
@@ -52,7 +52,7 @@ class TourEloquentRepository extends EloquentRepository implements TourRepositor
 	        ->select('tours.*')
 	        ->where(['destinations.cate_id'=>$id])
 	        ->paginate($limit);
-	        return $dataTourCate;
+	        return $dataAllTourCate;
 	}
 
 	/**
@@ -80,7 +80,7 @@ class TourEloquentRepository extends EloquentRepository implements TourRepositor
 		$dataShowAllTourDestination = DB::table('tours')
 	        ->where(['destination_id' => $id])
 	        ->paginate($limit);
-	        return $dataShowTourDestination;
+	        return $dataShowAllTourDestination;
 	}
 
     /**
